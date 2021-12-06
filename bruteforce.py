0a72d709303b5334916b82f39f33b9234c8dc971
Adrean <adrean337@gmail.com>
2021-12-06 16:07:16 +0100

    améliorations des algorithmes

--- bruteforce.py
+++ bruteforce.py
@@ -1,66 +1,62 @@
 import csv
-from pathlib import Path
 import itertools
+import sys
+import timeit
 
-absolute_path = Path(__file__).absolute().parent
+start = timeit.default_timer()
 
 class Action:
     def __init__(self,name,price,profit):
         self.name = name
         self.price = price
         self.profit = profit/100    
-        self.total_profit = self.price*self.profit    
-        
-def find_good_ones(liste_actions):
-    l_total = []
-    for i in range(0,len(liste_actions)):
-        for x in itertools.combinations(list(liste_actions),i):
-            price_list = [action.price for action in x]
-            s = sum(price_list)
-            if s == 500:
-                l_total.append(x)
-    return l_total
+        self.benefice = self.price*self.profit
+        self.market_value = self.benefice + self.price
 
-def find_best_one(good_ones_list):
-    best_one = []
-    good_ones = find_good_ones(good_ones_list)
-    for l in good_ones:
-        profit = sum([action.total_profit for action in l])
-        if profit > sum([action.total_profit for action in best_one]):
-            best_one = l
-    return best_one
+
+# Fonction cherchant la liste d'action la plus rentable
+def find_best_one(liste_actions):
+
+    most_profitable_liste = []
+    for actions_quantity in range(0,len(liste_actions)):
+        # Création de toutes les solutions possibles
+        for combination in itertools.combinations(list(liste_actions),actions_quantity):
+            price_list = [action.price for action in combination]
+            s = sum(price_list)
+            # Si les solutions ont un coût de 500€ ou moins
+            if s <= int(sys.argv[2]):
+                # Elles se trient sur leurs benefice
+                if sum([action.benefice for action in combination]) > sum([action.benefice for action in most_profitable_liste]):
+                    most_profitable_liste = combination
+
+    return most_profitable_liste
 
 def main():
 
-    # l short for liste
-    l_actions = []
-
-    with open(absolute_path/"dataset_bruteforce.csv",mode ="r") as Actions:
-        reader = csv.reader(Actions,delimiter=";")
+    liste_actions = []
+    # Lecture du fichier contenant les actions
+    with open(sys.argv[1],mode ="r") as fh:
+        reader = csv.reader(fh,delimiter=",")
+        next(reader,None)
         for action in reader:
             name = action[0]
-            price = int(action[1])
-            profit = int(action[2].strip("%"))
-            l_actions.append(Action(name,price,profit))
+            price = float(action[1])
+            profit = float(action[2])
+            if price <=0 or profit <=0:
+                pass
+            else: liste_actions.append(Action(name,price,profit))
 
-    best_one = find_best_one(l_actions)
-    profit = [action.profit*action.price for action in best_one]
-    print(sum(profit))
+    best_one = find_best_one(liste_actions)
+
+    # Résultat final
+    print("Bénéfice réalisé : ",sum([action.benefice for action in best_one]))
+    print("Coût total: ",sum([action.price for action in best_one]))
+    print("Capital final : ", sum([action.benefice for action in best_one]) + int(sys.argv[2]))
+    print([action.name for action in best_one])
+
+    stop = timeit.default_timer()
+    print("time: ",stop - start)
  
 
-main()
-
-
-#  iterable = toutes les combinaisons actions qui utilisent 
-#  (iterable, maximum_spent)
-
-
-# itérer sur toutes les actions
-# avec une limite de 500€ dépensés
-# result = itertools.combinations(actions_list)
-
-
-# bon résultat = 98.8
-# Chercher bruteforce
-# Itertools
-# Attention aux 500€ MAXIMUM
+if __name__ == "__main__":
+    main()
--- optimized.py
+++ optimized.py
@@ -1,7 +1,7 @@
 import csv
-from pathlib import Path
-import itertools
-absolute_path = Path(__file__).absolute().parent
+import sys
+import timeit
+start = timeit.default_timer()
 
 
 class Action:
@@ -9,76 +9,47 @@ class Action:
         self.name = name
         self.price = price
         self.profit = profit/100
-        self.benefice_brut = self.price * self.profit
-        self.profit_percentage = (self.benefice_brut/self.price)*100
-        self.benefice_net = self.price + self.benefice_brut
+        self.benefice = self.price * self.profit
+        self.market_value = self.price + self.benefice
+        self.ratio_prix_benefice = self.market_value/self.benefice
 
-
-# liste d'actions
-# 
-# 
-# 
-# trouver les listes d'actions qui utilisent 500€ max
-# comment trouver les 500 sans bruteforce toutes les possibilités ? 
-# 
-# 
-# 
-# 
-# 
-
-def sum_action_price(liste,attr):
-    desired_liste = []
-    for action in liste:
-        desired_liste.append(action.attr)
-        action_price = sum([action.benefice_brut for action in liste])
-        print(action_price)
 
 def calculate_profit(liste_actions):
 
-    argent = 500
-    sorted_profit = sorted(liste_actions, key = lambda action : action.profit, reverse=True)
-    # Récupère les 5 meilleurs profits
-    four_best_profit = sorted_profit[0:4]
-    # "dépense" l'argent des actions
-    argent -= sum([action.price for action in four_best_profit])
-    [liste_actions.remove(action) for action in four_best_profit]
+    liste_actions = sorted(liste_actions,key = lambda action : action.ratio_prix_benefice)
+    capital = int(sys.argv[2])
+    confirmed_actions = []
 
+    for action in liste_actions:
+        if capital - action.price >= 0:
+            capital -= action.price
+            confirmed_actions.append(action)
 
-    l_total = []
-    for i in range(0,(len(liste_actions)-len(four_best_profit))):
-        for x in itertools.combinations(list(liste_actions),i):
-            price_list = [action.price for action in x]
-            s = sum(price_list)
-            if s == argent:
-                l_total.append(x)
-    
-    best_benefice = []
-    true_profit = 0
-    # trie par benefice brut
-    for l_actions in l_total:
-        profit = sum([action.benefice_brut for action in l_actions])
-        if profit > true_profit:
-            true_profit = profit
-            # attribue la liste qui a le meilleur benefice
-            best_benefice = l_actions
+    return confirmed_actions
 
-    big_winner = list(best_benefice) + four_best_profit
-    return big_winner
 
 def main():
 
-    # l short for liste
-    l_actions = []
+    liste_actions = []
 
-    with open(absolute_path/"dataset_bruteforce.csv",mode ="r") as Actions:
-        reader = csv.reader(Actions,delimiter=";")
+    with open(sys.argv[1],mode ="r") as fh:
+        reader = csv.reader(fh,delimiter=",")
+        next(reader,None)
         for action in reader:
             name = action[0]
-            price = int(action[1])
-            profit = int(action[2].strip("%"))
-            l_actions.append(Action(name,price,profit))
+            price = float(action[1])
+            profit = float(action[2])
+            if price <=0 or profit <=0:
+                pass
+            else: liste_actions.append(Action(name,price,profit))
+                        
 
-    winner = calculate_profit(l_actions)
-    sum_action_price(winner)
+    most_profitable_actions = calculate_profit(liste_actions)
+    print(f"Coût total:  {sum([action.price for action in most_profitable_actions])}\nBénéfice réalisé : {sum([action.benefice for action in most_profitable_actions])}\nCapital final :  {sum([action.benefice for action in most_profitable_actions]) + int(sys.argv[2])}")
+    print([action.name for action in most_profitable_actions])
+    stop = timeit.default_timer()
+    print("time: ",stop - start)
 
-main()
+
+if __name__ == "__main__":
+    main()
